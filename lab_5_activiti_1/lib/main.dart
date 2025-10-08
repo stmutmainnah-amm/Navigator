a18502e26b1adffe1fa4a5854cede1fc7aec09ef
stmutmainnah <smutmainnah020627@gmail,com>
2025-10-08 16:14:07 +0800

    Extensi (opsional)

--- lab_5_activiti_1/lib/main.dart
+++ lab_5_activiti_1/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => HomeScreen(),
         '/details': (context) => DetailsScreen(),
         '/login': (context) => LoginScreen(),
+        '/profile': (context) => ProfileScreen(), // tambahin profile route
       },
     );
   }
@@ -40,15 +41,31 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Home Screen')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(
-              context,
-              '/details',
-              arguments: 'Halo dari Home Screen!',
-            );
-          },
-          child: Text('Open Details'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/details',
+                  arguments: 'Halo dari Home Screen!',
+                );
+              },
+              child: Text('Open Details'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/profile',
+                  arguments: 'kasmir', // contoh username
+                );
+              },
+              child: Text('Go to Profile'),
+            ),
+          ],
         ),
       ),
     );
@@ -73,6 +90,26 @@ class DetailsScreen extends StatelessWidget {
               child: Text('Back to Home'),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final username = ModalRoute.of(context)!.settings.arguments as String?;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Profile Screen')),
+      body: Center(
+        child: Text(
+          username != null
+              ? 'Wow, benar banget! $username memang tampan sekali!'
+              : 'Tidak ada data pengguna',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
         ),
       ),
     );
